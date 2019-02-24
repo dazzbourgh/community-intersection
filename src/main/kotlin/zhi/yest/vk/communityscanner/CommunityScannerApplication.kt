68ef465fa3148ecb7dd6b2b8a8d8277999247d08
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-02-24 03:35:45 +0300

    turned CORS off

--- src/main/kotlin/zhi/yest/vk/communityscanner/CommunityScannerApplication.kt
+++ src/main/kotlin/zhi/yest/vk/communityscanner/CommunityScannerApplication.kt
@@ -2,9 +2,24 @@ package zhi.yest.vk.communityscanner
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite
 
 @SpringBootApplication
-class CommunityScannerApplication
+class CommunityScannerApplication {
+    // Turn off CORS
+    @Bean
+    fun corsConfigurer(): WebFluxConfigurer {
+        return object : WebFluxConfigurerComposite() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CommunityScannerApplication>(*args)

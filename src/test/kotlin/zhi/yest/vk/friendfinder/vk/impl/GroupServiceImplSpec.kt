eee0d517fd3371b9b2b6565dac8bc6672723189c
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-04-22 00:25:05 +0300

    correct spek versions & repo test templates added

--- src/test/kotlin/zhi/yest/vk/friendfinder/vk/impl/GroupServiceImplSpec.kt
+++ src/test/kotlin/zhi/yest/vk/friendfinder/vk/impl/GroupServiceImplSpec.kt
@@ -1,6 +1,11 @@
 package zhi.yest.vk.friendfinder.vk.impl
 
-internal class GroupServiceImplSpec {
-    fun getMembers() {
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object GroupServiceImplSpec : Spek({
+    describe("adding numbers") {
+        it("adds 2 to 2") {
+        }
     }
-}
+})

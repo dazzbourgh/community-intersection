e9a4aa296d3aae62438f469738206c98688acfe8
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-05-13 01:41:11 +0300

    more fixes to build returned to publisher scope since Spring doesn't support suspending controllers just yet

--- src/main/kotlin/zhi/yest/vk/friendfinder/controller/people/PeopleController.kt
+++ src/main/kotlin/zhi/yest/vk/friendfinder/controller/people/PeopleController.kt
@@ -1,8 +1,8 @@
 package zhi.yest.vk.friendfinder.controller.people
 
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.reactive.publish
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,19 +10,26 @@ import org.springframework.web.bind.annotation.RestController
 import zhi.yest.vk.friendfinder.domain.Request
 import zhi.yest.vk.friendfinder.vk.DelayingRequestSender
 import zhi.yest.vk.friendfinder.vk.UserService
+import kotlin.coroutines.EmptyCoroutineContext
 
+@ExperimentalCoroutinesApi
 @RestController
 @RequestMapping("people")
 class PeopleController(private val userService: UserService,
                        private val delayingRequestSender: DelayingRequestSender) {
-    @FlowPreview
+    private val scope = CoroutineScope(EmptyCoroutineContext)
+
     @PostMapping(produces = ["application/stream+json"])
-    suspend fun findInteresting(@RequestBody request: Request) = request.groupIds
-            .flatMap {
-                delayingRequestSender.request {
-                    userService.search(it, request.fields)
+    fun findInteresting(@RequestBody request: Request) = scope.publish {
+        request.groupIds
+                .flatMap {
+                    delayingRequestSender.request {
+                        userService.search(it, request.fields)
+                    }
                 }
-            }
-            .groupBy { it.id }[request.groupIds.size]
-            ?.asFlow() ?: emptyFlow()
+                .groupingBy { it }
+                .eachCount()
+                .filter { it.value == request.groupIds.size }
+                .forEach { (user, _) -> send(user) }
+    }
 }

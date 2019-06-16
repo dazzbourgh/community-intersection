3b1246329429a7ef22c159ae551faf7dfed23fd0
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-06-16 14:50:09 +0300

    added config server & discovery service

---
+++ config-service/src/main/kotlin/zhi/yest/communityintersection/configservice/ConfigServiceApplication.kt
@@ -0,0 +1,13 @@
+package zhi.yest.communityintersection.configservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigServiceApplication>(*args)
+}

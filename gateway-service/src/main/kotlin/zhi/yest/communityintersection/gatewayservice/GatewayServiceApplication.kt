8a10f15e5ae70e5dc9d12422a9e2769f26c303bb
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-07-07 01:06:25 +0300

    init gateway service

---
+++ gateway-service/src/main/kotlin/zhi/yest/communityintersection/gatewayservice/GatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package zhi.yest.communityintersection.gatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayServiceApplication>(*args)
+}

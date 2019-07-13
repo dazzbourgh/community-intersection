adb2a55af9a463a985fcc71334b990ddf7dff93c
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-07-13 16:52:46 +0300

    added discovery to gateway service

--- gateway-service/src/main/kotlin/zhi/yest/communityintersection/gatewayservice/GatewayServiceApplication.kt
+++ gateway-service/src/main/kotlin/zhi/yest/communityintersection/gatewayservice/GatewayServiceApplication.kt
@@ -2,8 +2,10 @@ package zhi.yest.communityintersection.gatewayservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayServiceApplication
 
 fun main(args: Array<String>) {
